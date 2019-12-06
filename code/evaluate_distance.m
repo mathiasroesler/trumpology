9607d6a38b038e14a65ce2bf346a57c97900fff3
mathiasroesler <mathias.roesler@etu.upmc.fr>
2019-12-06 10:08:24 +0100

    Added a function to evaluate the distance of different sets and modified the main function to fit changes

---
+++ code/evaluate_distance.m
@@ -0,0 +1,53 @@
+function distance = evaluate_distance(dir, corpora, coeffs_nb, set_choice, people)
+%EVALUATE_DISTANCE Evaluates the distance between the train data and the validation
+%data or the training data, depending on the value of set_choice. 
+%   Input: -dir, str, location of the different sets.
+%          -corpora, str vector 1xC, names of the different corpora.
+%          -coeffs_nb, int, number of coefficiants for the MFCC.
+%          -set_choice, 0 or 1, 0: validation set, 1: training set.
+%          -people, str matrix CxN (only if set_choice is 1), matrix
+%          containing the names of the people in each corpus. 
+%   Output: -distance, matrix CxN, if people is not specified then N=1,
+%   each element is the Bhattacharyya distance of a person or corpus
+%   depending on the value of set_choice.
+
+if (nargin == 4 && set_choice == 0)
+    % The evaluation is done for one or several validation sets
+    distance = zeros(size(corpora));
+
+elseif (nargin == 5 && set_choice == 1)
+    % The evaluation is done for one or several training sets
+    distance = zeros(length(corpora), size(people, 1));
+    
+else
+    % Error in the arguments
+    error('Error with input arguments');
+end   
+
+% Load the training set
+train_data = extract_data(strcat(dir, '/train'), coeffs_nb);
+
+for corpus_nb = 1:length(corpora)
+        switch set_choice
+            case 0
+                % Load validation set
+                set = 'valid';
+                corpus = corpora(corpus_nb);
+                
+                valid_data = extract_data(strcat('../data/', set, '/', corpus), coeffs_nb);
+                distance(corpus_nb) = mean(bhattacharyya(train_data, valid_data));
+                
+            case 1
+                for person_nb = 1:size(people, 1)
+                    % Load testing set
+                    set = 'test';
+                    corpus = corpora(corpus_nb);
+                    person = people(person_nb, corpus_nb);
+                
+                    test_data = extract_data(strcat('../data/', set, '/', corpus, '/', person), coeffs_nb);
+                    distance(corpus_nb, person_nb) = mean(bhattacharyya(train_data, test_data));
+                    
+                end
+        end
+end
+
--- code/main.m
+++ code/main.m
@@ -1,85 +1,52 @@
 % Main file for the trumpology project
 clear all
-% close all
+close all
 
 % Global variables
 set_choice = 1; % Set choice 0: validation sets, 1: testing sets
 coeffs_nb = 7; % Number of coefficiants for the mfcc
-corpus_nb = 5; % Choice of corpus
-person_nb = 1; % Choice of person
-max_coeffs_nb = 30; % Maximum number of coefficants for the mfcc
+max_coeffs_nb = 4; % Maximum number of coefficants for the mfcc (Cannot be greater than 40)
 plot_nb = 1; % Plot number
 
-
+dir = '../data';
 corpora = ["french", "imitators", "others", "speeches", "trump", "women"];
 people = ["chirac", "baldwin", "colbert", "bush", "", "clinton";
     "hollande", "di_domenico", "meyers", "obama", "", "harris";
     "macron", "fallon", "supercarlin", "sanders", "", "pelosi";
     "sarkozy", "noah", "veitch", "schiff", "", "warren"];
-colors = ["k"; "r"; "b"; "y"; "g"; "c"]; % Colors for plots for all training sets
-plots = zeros(1, numel(people));    % Plot objects for every person
-d_coeffs = zeros(length(corpora), max_coeffs_nb);
+d_coeffs = zeros(length(corpora), max_coeffs_nb-1);
 
-
-% for coeffs_nb = 2:max_coeffs_nb+1
-% Load the training set
-train_data = extract_data('../data/train', coeffs_nb);
-
-for corpus_nb = 1:6
-    for person_nb = 1:4
-        switch set_choice
-            case 0
-                % Load validation sets
-                set = 'valid';
-                corpus = corpora(corpus_nb);
-                
-                valid_data = extract_data(strcat('../data/', set, '/', corpus), coeffs_nb);
-                
-            case 1
-                % Load testing sets
-                set = 'test';
-                corpus = corpora(corpus_nb);
-                person = people(person_nb, corpus_nb);
-                
-                test_data = extract_data(strcat('../data/', set, '/', corpus, '/', person), coeffs_nb);
-                
-        end
-        
-        %     % Distance calculation for different number of coefficiants
-        %     d_coeffs(corpus_nb, coeffs_nb-1) = mean(bhattacharyya(train_data, valid_data));
-        
-        %     % Distance calculation for the validation sets
-        %     d_valid = mean(bhattacharyya(train_data, valid_data));
-        %
-        %     % Plot for the validation sets
-        %     plot(corpus_nb, d_valid, '.', 'MarkerSize', 30); hold on; grid on;
-        
-        % Distance calculation for the testing sets
-        d_test = mean(bhattacharyya(train_data, test_data));
-        
-        % Plot for all the testing sets
-        plots(plot_nb) = plot(person_nb, d_test, strcat('.', colors(corpus_nb)), 'MarkerSize', 30); hold on; grid on;
-        plot_nb = plot_nb+1;
-        
-    end
-    
-end
+% for coeffs_nb = 2:max_coeffs_nb
+%     % Testing for different number of MFCC coefficants on the validation set
+%     d_coeffs(:, coeffs_nb-1) = evaluate_distance(dir, corpora, coeffs_nb, 0);
 % end
 
 % % Plot, legend and labels for different number of coefficiants
-% plot(2:max_coeffs_nb+1, d_coeffs, '*--');
+% plot(2:max_coeffs_nb, d_coeffs, '*--');
 % xlabel('Nombre de coefficients'); ylabel('Distance de Bhattacharyya');
 % legend(corpora, 'Location', 'Best');
 
-% % Legend and labels for the validation sets
-% xlabel('Corpus'); ylabel('Distance de Bhattacharyya');
-% legend(corpora);
 
-% % Legend and labels for one testing set
-% xlabel('Personnes'); ylabel('Distance de Bhattacharyya');
-% legend(people(:, corpus_nb));
+% Evaluation and plotting
+if (set_choice == 0)
+    % For validation sets
+    distance = evaluate_distance(dir, corpora, coeffs_nb, set_choice);
+    
+    for j = 1:length(distance)
+        plot(j, distance(j), '.', 'MarkerSize', 30); hold on; grid on;
+    end
+    
+    xlabel('Corpus'); ylabel('Distance de Bhattacharyya');
+    legend(corpora, 'Location', 'Best');
+    
+elseif (set_choice == 1)
+    % For testing sets
+    distance = evaluate_distance(dir, corpora, coeffs_nb, set_choice, people);
+    
+    plot(1:size(people, 1), distance, '.', 'MarkerSize', 30); grid on;
+    xlabel('Personnes'); ylabel('Distance de Bhattacharyya');
+    legend(corpora, 'Location', 'Best');
+end
+    
 
-% Legend and labels for all of the testing sets
-xlabel('Personnes'); ylabel('Distance de Bhattacharyya');
-legend(plots(1:size(people, 1):numel(people)), corpora);
 
